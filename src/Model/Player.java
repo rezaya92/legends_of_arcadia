3bf560c2ef7a266dec72b8ff7fe7b53f407ba740
Soroush <soroushtaslimi@yahoo.com>
2018-04-30 18:16:32 +0430

    Player added addition in View

---
+++ src/Model/Player.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Player {
+    private Card[] cards;
+    private Shop shop;
+    private int gil;
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public void setShop(Shop shop) {
+        this.shop = shop;
+    }
+
+    public int getGil() {
+        return gil;
+    }
+
+    public void setGil(int gil) {
+        this.gil = gil;
+    }
+}
--- src/View/View.java
+++ src/View/View.java
@@ -1,4 +1,17 @@
 package View;
 
-public class View {
+abstract public class View {
+    public static void afterMatch(){
+        System.out.println("1. Enter Shop: To enter shop and buy or sell cards and items");
+        System.out.println("2. Edit Inventory: To edit your amulet or deck");
+        System.out.println("3. Next: To go to deck and amulet customization");
+    }
+
+    public static void shopEnter(){
+        System.out.println("Remaining Gil: " + Controller.Main.human.getGil() + " Gil");
+        System.out.println("1. Card Shop");
+        System.out.println("2. Item Shop");
+        System.out.println("3. Amulet Shop");
+        System.out.println("4. Exit\n");
+    }
 }
